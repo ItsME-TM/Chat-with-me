af9dc93d06cf7d7d89aa4e735cfbad64b9d22d45
ItsME-TM <thushanmalaka123@gmail.com>
2025-11-03 15:00:56 +0530

    Setup the production commands and environments

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
+const _dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+//check if the production ready add the path for static frontend assets
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname, "../frontend/dist")))
+    app.get("*", (res, req) => {
+        res.sendFile(path.join(_dirname, "../frontend/dist/index.html"))
+    })
+}
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
